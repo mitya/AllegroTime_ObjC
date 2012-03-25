bd2467a443fbc55bab306266eaae6804bcd4a40b
Dmitry Sokurenko <dmitry@sokurenko.name>
2012-03-25 16:45:22 +0400

    Initial Commit

---
+++ AllegroTimeTests/AllegroTimeTests.h
@@ -0,0 +1,13 @@
+//
+//  AllegroTimeTests.h
+//  AllegroTimeTests
+//
+//  Created by Dmitry Sokurenko on 25.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AllegroTimeTests : SenTestCase
+
+@end
---
+++ AllegroTimeTests/AllegroTimeTests.m
@@ -0,0 +1,32 @@
+//
+//  AllegroTimeTests.m
+//  AllegroTimeTests
+//
+//  Created by Dmitry Sokurenko on 25.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AllegroTimeTests.h"
+
+@implementation AllegroTimeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AllegroTimeTests");
+}
+
+@end
