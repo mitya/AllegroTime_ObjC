98fc64cf97870bd293902517c3622fee35bfc953
Dmitry Sokurenko <dmitry@sokurenko.name>
2012-04-18 14:27:30 +0400

    Remove unused code

--- AllegroTime/Models.m
+++ AllegroTime/Models.m
@@ -241,9 +241,6 @@
   NSString *dataString = [NSString stringWithContentsOfFile:dataPath encoding:NSUTF8StringEncoding error:NULL];
   NSArray *dataRows = [dataString componentsSeparatedByString:@"\n"];
   
-  NSLog(dataPath);
-  NSLog(@"len = %i", dataString.length);
-
   for (NSString *dataRow in dataRows) {
     NSArray *components = [dataRow componentsSeparatedByString:@","];
 
@@ -273,108 +270,108 @@
   }
 }
 
-- (void) loadPredefinedData {
-  // [Crossing crossingWithName:@"Солнечное" latitude:60.15951 longitude:29.937442], // не существует
-  // [Crossing crossingWithName:@"Ушково" latitude:60.221009 longitude:29.623949],  // не существует
-  crossings = [NSMutableArray arrayWithObjects:
-      [Crossing crossingWithName:@"Удельная" latitude:60.017533 longitude:30.313379],
-      [Crossing crossingWithName:@"Поклонногорская" latitude:60.025533 longitude:30.309113],
-      [Crossing crossingWithName:@"Озерки - Шувалово" latitude:60.042087 longitude:30.300095],
-      [Crossing crossingWithName:@"Дорога на Каменку" latitude:60.070331 longitude:30.275285],
-      [Crossing crossingWithName:@"Парголово" latitude:60.079674 longitude:30.260536],
-      [Crossing crossingWithName:@"Песочная" latitude:60.118323 longitude:30.147631],
-      [Crossing crossingWithName:@"Дибуны" latitude:60.121706 longitude:30.130231],
-      [Crossing crossingWithName:@"Белоостровское шоссе" latitude:60.146049 longitude:30.006676],
-      [Crossing crossingWithName:@"Белоостров" latitude:60.134852 longitude:30.063732],
-      [Crossing crossingWithName:@"Репино" latitude:60.174512 longitude:29.860736],
-      [Crossing crossingWithName:@"Комарово" latitude:60.186113 longitude:29.800448],
-      [Crossing crossingWithName:@"Рощинское шоссе" latitude:60.227382 longitude:29.612833],
-      [Crossing crossingWithName:@"Рощино" latitude:60.251283 longitude:29.572106],
-      [Crossing crossingWithName:@"Горьковское" latitude:60.294546 longitude:29.493518],
-      nil];
-
-
-  // http://ozd.rzd.ru/static/public/ozd?STRUCTURE_ID=4735
-  closings = [NSMutableArray arrayWithObjects:
-      [Closing closingWithCrossingName:@"Удельная" time:@"06:46" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Удельная" time:@"11:32" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Удельная" time:@"15:32" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Удельная" time:@"20:32" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Удельная" time:@"10:36" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Удельная" time:@"14:21" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Удельная" time:@"19:21" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Удельная" time:@"23:21" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"06:47" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"11:33" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"15:33" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"20:33" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"10:35" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"14:20" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"19:20" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Поклонногорская" time:@"23:20" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"06:50" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"11:34" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"15:34" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"20:34" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"10:30" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"14:19" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"19:19" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"23:19" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"06:51" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"11:35" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"15:35" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"20:35" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"10:27" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"14:15" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"19:15" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"23:15" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Парголово" time:@"06:52" direction:ClosingDirectionToFinland], // extrapolated
-      [Closing closingWithCrossingName:@"Парголово" time:@"11:36" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Парголово" time:@"15:36" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Парголово" time:@"20:36" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Парголово" time:@"10:26" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Парголово" time:@"14:14" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Парголово" time:@"19:14" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Парголово" time:@"23:14" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Песочная" time:@"06:56" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Песочная" time:@"11:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Песочная" time:@"15:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Песочная" time:@"20:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Песочная" time:@"10:21" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Песочная" time:@"14:09" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Песочная" time:@"19:09" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Песочная" time:@"23:09" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Дибуны" time:@"06:56" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"11:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"15:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"20:40" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"10:21" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"14:09" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"19:09" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Дибуны" time:@"23:09" direction:ClosingDirectionToRussia],
-
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"06:58" direction:ClosingDirectionToFinland], // extrapolated
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"11:42" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"15:42" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"20:42" direction:ClosingDirectionToFinland],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"10:19" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"14:07" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"19:07" direction:ClosingDirectionToRussia],
-      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"23:07" direction:ClosingDirectionToRussia],
-
-      nil];
-
-  for (Crossing *crossing in crossings) {
-    [crossing.closings sortUsingComparator:^NSComparisonResult(Closing *obj1, Closing *obj2) {
-      return [Helper compareInteger:obj1.timeInMinutes with:obj2.timeInMinutes];
-    }];
-  }
-}
+//- (void) loadPredefinedData {
+//  // [Crossing crossingWithName:@"Солнечное" latitude:60.15951 longitude:29.937442], // не существует
+//  // [Crossing crossingWithName:@"Ушково" latitude:60.221009 longitude:29.623949],  // не существует
+//  crossings = [NSMutableArray arrayWithObjects:
+//      [Crossing crossingWithName:@"Удельная" latitude:60.017533 longitude:30.313379],
+//      [Crossing crossingWithName:@"Поклонногорская" latitude:60.025533 longitude:30.309113],
+//      [Crossing crossingWithName:@"Озерки - Шувалово" latitude:60.042087 longitude:30.300095],
+//      [Crossing crossingWithName:@"Дорога на Каменку" latitude:60.070331 longitude:30.275285],
+//      [Crossing crossingWithName:@"Парголово" latitude:60.079674 longitude:30.260536],
+//      [Crossing crossingWithName:@"Песочная" latitude:60.118323 longitude:30.147631],
+//      [Crossing crossingWithName:@"Дибуны" latitude:60.121706 longitude:30.130231],
+//      [Crossing crossingWithName:@"Белоостровское шоссе" latitude:60.146049 longitude:30.006676],
+//      [Crossing crossingWithName:@"Белоостров" latitude:60.134852 longitude:30.063732],
+//      [Crossing crossingWithName:@"Репино" latitude:60.174512 longitude:29.860736],
+//      [Crossing crossingWithName:@"Комарово" latitude:60.186113 longitude:29.800448],
+//      [Crossing crossingWithName:@"Рощинское шоссе" latitude:60.227382 longitude:29.612833],
+//      [Crossing crossingWithName:@"Рощино" latitude:60.251283 longitude:29.572106],
+//      [Crossing crossingWithName:@"Горьковское" latitude:60.294546 longitude:29.493518],
+//      nil];
+//
+//
+//  // http://ozd.rzd.ru/static/public/ozd?STRUCTURE_ID=4735
+//  closings = [NSMutableArray arrayWithObjects:
+//      [Closing closingWithCrossingName:@"Удельная" time:@"06:46" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"11:32" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"15:32" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"20:32" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"10:36" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"14:21" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"19:21" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Удельная" time:@"23:21" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"06:47" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"11:33" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"15:33" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"20:33" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"10:35" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"14:20" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"19:20" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Поклонногорская" time:@"23:20" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"06:50" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"11:34" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"15:34" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"20:34" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"10:30" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"14:19" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"19:19" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Озерки - Шувалово" time:@"23:19" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"06:51" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"11:35" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"15:35" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"20:35" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"10:27" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"14:15" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"19:15" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дорога на Каменку" time:@"23:15" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Парголово" time:@"06:52" direction:ClosingDirectionToFinland], // extrapolated
+//      [Closing closingWithCrossingName:@"Парголово" time:@"11:36" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"15:36" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"20:36" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"10:26" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"14:14" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"19:14" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Парголово" time:@"23:14" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Песочная" time:@"06:56" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"11:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"15:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"20:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"10:21" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"14:09" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"19:09" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Песочная" time:@"23:09" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"06:56" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"11:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"15:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"20:40" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"10:21" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"14:09" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"19:09" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Дибуны" time:@"23:09" direction:ClosingDirectionToRussia],
+//
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"06:58" direction:ClosingDirectionToFinland], // extrapolated
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"11:42" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"15:42" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"20:42" direction:ClosingDirectionToFinland],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"10:19" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"14:07" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"19:07" direction:ClosingDirectionToRussia],
+//      [Closing closingWithCrossingName:@"Белоостровское шоссе" time:@"23:07" direction:ClosingDirectionToRussia],
+//
+//      nil];
+//
+//  for (Crossing *crossing in crossings) {
+//    [crossing.closings sortUsingComparator:^NSComparisonResult(Closing *obj1, Closing *obj2) {
+//      return [Helper compareInteger:obj1.timeInMinutes with:obj2.timeInMinutes];
+//    }];
+//  }
+//}
 
 @end
